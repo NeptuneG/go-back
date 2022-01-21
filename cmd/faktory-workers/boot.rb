cc6487489320c81d8ab1128735b94b57cc269cff
goss.beta <goss.beta@gmail.com>
2022-01-22 02:00:26 +0900

    introduce yabeda to adapt prometheus for faktory-workers

--- cmd/faktory-workers/boot.rb
+++ cmd/faktory-workers/boot.rb
@@ -2,5 +2,10 @@
 
 require 'faktory'
 require 'redis'
+require 'yabeda/prometheus'
 
-Dir[File.join(".", "**/*.rb")].each { |file| require file }
+Dir[File.join('.', '**/*.rb')].sort.each { |file| require file }
+
+Faktory.configure_worker do |_config|
+  Yabeda::Prometheus::Exporter.start_metrics_server!
+end
